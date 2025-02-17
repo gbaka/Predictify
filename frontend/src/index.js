c458d4e6212285a6840ab08465fa60bf7c6886f2
gbaka <geo.m2019@mail.ru>
2025-02-17 04:23:37 +0300

    The initial structure of the front

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './styles.css';  // подключаем стили, если нужно
+
+function App() {
+  return (
+    <div>
+      <h1>Привет, мир!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
